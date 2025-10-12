b6e997ccd4410fa45d28dd35a1ed2c847a86af34
ZacharyBachary <108705122+ZacharyBachary@users.noreply.github.com>
2025-10-12 17:50:27 -0600

    Update users.js

--- users.js
+++ users.js
@@ -1,347 +1,38 @@
 const users = {
-    "eenot": {
-        "id": "eenot",
-        "discord": "ЕЕнот / RRaccoon",
-        "discordid": "794675642037567498",
-        "gamename": "eenot (rracc)",
-        "awards": [
-            "eurasiaModern_super",
-            "euammodern_former",
-        ],
-        "events": [""]
-    },
-    "trid": {
-        "id": "trid",
-        "discord": "Trid",
-        "discordid": "993585215358386306",
-        "gamename": "trid (3d)",
-        "awards": [
-            "crimeanwar_global",
-        ],
-        "events": [""]
-    },
-    "totoska": {
-        "id": "totoska",
-        "discord": "Totoska",
-        "discordid": "820194328201920524",
-        "gamename": "totoska",
-        "awards": [
-            "crimeanwar_super",
-            "owbcaribean_great",
-            "feudalismac_global",
-            "euoemodern_winner",
-            "world1861_global",
-            "eurasiaww2_winner",
-            "worldkaiserreichp2_winner",
-            "euammodern_winner",
-            "eurasia6coalition_global",
-        ],
-        "events": [""]
-    },
-    "tradingco": {
-        "id": "tradingco",
-        "discord": "",
-        "discordid": "",
-        "gamename": "",
-        "awards": [
-            "eurasia6coalition_global",
-        ],
-        "events": [""]
-    },
-    "ottomanism": {
-        "id": "ottomanism",
-        "discord": "Ottomanism",
-        "discordid": "1283840564839579701",
-        "gamename": "mrvoid",
-        "awards": [
-            "eurasiaModern_super",
-            "crimeanwar_winner",
-            "owbcaribean_global",
-            "feudalismac_great",
-            "world1861_global",
-            "eurasiaww2_global",
-            "worldkaiserreichp2_global",
-            "euammodern_global",
-        ],
-        "events": [""]
-    },
     "zachary": {
         "id": "zachary",
-        "discord": "Zachary",
+        "discord": "zachary.bachary",
         "discordid": "804839006403428423",
-        "gamename": "zachary",
+        "gamename": "ZacharyBachary",
         "awards": [
             "eurasiaModern_winner",
-            "crimeanwar_super",
-            "owbcaribean_winner",
-            "feudalismac_winner",
-            "euoemodern_global",
-            "world1861_winner",
-            "eurasiaww2_global",
-            "worldkaiserreichp2_global",
-            "euammodern_global",
-            "eurasia6coalition_winner",
-            "ussr_global",
-            "worldww2_winner",
-        ],
-        "events": [""]
-    },
-    "penguin": {
-        "id": "penguin",
-        "discord": "Great Leader Penguin of the DPRP",
-        "discordid": "1260611002240860202",
-        "gamename": "",
-        "awards": [
-            "world1861_great",
-        ],
-        "events": [""]
-    },
-    "sindrld": {
-        "id": "sindrld",
-        "discord": "Sinwrld",
-        "discordid": "1285740433485467719",
-        "gamename": "",
-        "awards": [
-            "world1861_great",
-        ],
-        "events": [""]
-    },
-    "moad": {
-        "id": "moad",
-        "discord": "Moād",
-        "discordid": "846351504730619925",
-        "gamename": "moaad",
-        "awards": [
-            "eurasiaModern_great",
-            "crimeanwar_former",
-            "owbcaribean_great",
-            "feudalismac_great",
-            "euoemodern_global",
-            "world1861_global",
-            "worldkaiserreichp2_great",
-            "worldww2_winner",
-        ],
-        "events": [""]
-    },
-    "beary": {
-        "id": "beary",
-        "discord": "beary",
-        "discordid": "946239577096597504",
-        "gamename": "beary",
-        "awards": [
-            "eurasiaModern_great",
-        ],
-        "events": [""]
-    },
-    "inka": {
-        "id": "inka",
-        "discord": "Inka",
-        "discordid": "1050563653394505728",
-        "gamename": "Plagers2",
-        "awards": [
-            "owbcaribean_great",
-            "feudalismac_great",
-            "eurasiaww2_great",
-            "eurasia6coalition_great",
-            "worldww2_great",
-        ],
-        "events": [""]
-    },
-    "irishguy ": {
-        "id": "irishguy ",
-        "discord": "",
-        "discordid": "",
-        "gamename": "",
-        "awards": [
-            "eurasiaww2_great",
-        ],
-        "events": [""]
-    },
-    "psychopomp": {
-        "id": "nonpsychopompame2",
-        "discord": "PsychoPomp",
-        "discordid": "1314822053488230501",
-        "gamename": "PsychoPomp",
-        "awards": [],
-        "events": [""]
-    },
-    "tungg": {
-        "id": "tungg",
-        "discord": "",
-        "discordid": "",
-        "gamename": "tungg",
-        "awards": [],
-        "events": [""]
-    },
-    "trueruler": {
-        "id": "trueruler",
-        "discord": "",
-        "discordid": "",
-        "gamename": "TrueRuler",
-        "awards": [],
-        "events": [""]
-    },
-    "lurus": {
-        "id": "lurus",
-        "discord": "Lurus",
-        "discordid": "764533673512927242",
-        "gamename": "lurus",
-        "awards": [],
-        "events": [""]
-    },
-    "honse": {
-        "id": "honse",
-        "discord": "Honse",
-        "discordid": "938218628170522694",
-        "gamename": "honse",
-        "awards": [
-            "eurasiaModern_global",
-            "crimeanwar_great",
-        ],
-        "events": [""]
-    },
-    "mrgoxlem": {
-        "id": "mrgoxlem",
-        "discord": "Mrgoxlem",
-        "discordid": "1223823017004634206",
-        "gamename": "",
-        "awards": [
-            "eurasiaModern_former",
-            "feudalismac_global",
-        ],
-        "events": [""]
-    },
-    "eggnog": {
-        "id": "eggnog",
-        "discord": "Eggnog",
-        "discordid": "1306064701037215828",
-        "gamename": "",
-        "awards": [
-            "eurasiaModern_former",
-            "crimeanwar_former",
-            "owbcaribean_great",
-        ],
-        "events": [""]
-    },
-    "napoleon": {
-        "id": "napoleon",
-        "discord": "Napoleon",
-        "discordid": "1192203917757517825",
-        "gamename": "",
-        "awards": [
-            "crimeanwar_former",
-            "owbcaribean_global",
-        ],
-        "events": [""]
-    },
-    "stewardconstruct": {
-        "id": "stewardconstruct",
-        "discord": "Public nuisance",
-        "discordid": "1114590604668706927",
-        "gamename": "",
-        "awards": [
-            "euoemodern_great",
-        ],
-        "events": [""]
-    },
-    "mrpepper": {
-        "id": "mrpepper",
-        "discord": "mrpepper2000",
-        "discordid": "1262574507156045845",
-        "gamename": "",
-        "awards": [
-            "worldkaiserreichp2_great",
-            "euammodern_former",
-        ],
-        "events": [""]
-    },
-    "hamond": {
-        "id": "hamond",
-        "discord": "Hamod_2032",
-        "discordid": "1347239235530391552",
-        "gamename": "",
-        "awards": [
-            "eurasia6coalition_great",
-        ],
-        "events": [""]
-    },
-    "useless": {
-        "id": "useless",
-        "discord": "Inutile (Useless)",
-        "discordid": "1111413830443081728",
-        "gamename": "",
-        "awards": [
-            "eurasia6coalition_great",
-        ],
-        "events": [""]
-    },
-    "ultron": {
-        "id": "ultron",
-        "discord": "サイジャ (Ultron)",
-        "discordid": "1055578108318728202",
-        "gamename": "",
-        "awards": [
-            "ussr_global",
-        ],
-        "events": [""]
-    },
-    "spacedex": {
-        "id": "spacedex",
-        "discord": "Spacedex",
-        "discordid": "835532188841148477",
-        "gamename": "",
-        "awards": [
-            "ussr_winner",
-        ],
-        "events": [""]
-    },
-    "yeezy": {
-        "id": "yeezy",
-        "discord": "yeezy",
-        "discordid": "1279104218799538286",
-        "gamename": "",
-        "awards": [
-            "ussr_great",
-        ],
-        "events": [""]
-    },
-    "taroo": {
-        "id": "taroo",
-        "discord": "taroo",
-        "discordid": "1042451239729897482",
-        "gamename": "",
-        "awards": [
-            "ussr_great",
-        ],
-        "events": [""]
-    },
-    "grimreaper": {
-        "id": "grimreaper",
-        "discord": "GrimReaper54",
-        "discordid": "720328844464357446",
-        "gamename": "",
-        "awards": [
-            "worldww2_great",
-        ],
-        "events": [""]
-    },
-    "mako": {
-        "id": "mako",
-        "discord": "mako",
-        "discordid": "871025453434019860",
-        "gamename": "",
-        "awards": [
-            "worldww2_global",
-        ],
-        "events": [""]
-    },
-    "bernard": {
-        "id": "bernard",
-        "discord": "𝔅𝔢𝔯𝔫𝔞𝔯𝔡 (Bernard)",
-        "discordid": "1324611249551441961",
-        "gamename": "",
-        "awards": [
-            "worldww2_global",
+            "hre_winner",
+			"owbcaribean_winner",
+			"feudalismac_winner",
+			"world1861_winner",
+			"eurasia6coalition_winner",
+			"worldww2_winner",
+			"eurasia5_winner",
+			"mars_winner",
+			"usa_winner",
+			"eurasia2_winner",
+			"tigerland_winner",
+			"eurasia1_winner",
+			"crimeanwar_super",
+			"euammodern_global",
+			"eurasiaww2_global",
+			"worldkaiserreichp2_global",
+			"euammodern_global",
+			"ussr_global",
+			"tigerlandnew_global",
+			"usanew_global",
+			"paraguayanwar_global",
+			"euro4modern_global",
+			"eurasia3_global",
+			"historicalww1_global",
+			"africa_global",
+			"middleeast_global",
+			"ww2og_global",
         ],
         "events": [""]
     },
